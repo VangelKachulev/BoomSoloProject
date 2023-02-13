a54c1324f04b0e8c43310a5d81bf9720564aad20
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-13 17:36:04 +0200

    'test'

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Example from "../src/components/example/Example";
-import { Logo } from "../src/components/logo/Logo";
 
-let type = 'muted'
+
+
 export default function Index() {
 
-  return <Logo props={type} />;
+  return <Example />;
 }
